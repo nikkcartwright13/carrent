fa84e86b030df9c97643624af841728731076441
nikkcartwright13 <125912421+nikkcartwright13@users.noreply.github.com>
2024-06-10 11:03:35 +0400

    update cars

--- Data/data.module.js
+++ Data/data.module.js
@@ -20,6 +20,28 @@ module.exports = [
 
         ],
     },
+
+    {
+        "Price": 200,
+        "L100km": '10 Liter',
+        "class": "SUV",
+        "cylinders": 4,
+        "Engine": 2.4,
+        "drive": "fwd",
+        "fuel_type": "Gasoline",
+        "make": "Chrysler",
+        "model": "Cruiser",
+        "transmission": "Automatic",
+        "year": 2005,
+        "image": 'https://s3-eu-west-1.amazonaws.com/localrent.images/cars/image_titles/000/022/021/original/Chrysler-Cruiser-2005-red.jpg?1627467330',
+        "gallery": [
+            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/chrysler/cryslerred2.jpeg',
+            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/chrysler/cryslerred.jpeg',
+            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/chrysler/cryslerred1.jpeg',
+            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/chrysler/cryslerred4.jpeg'
+
+        ],
+    },
     {
         "Price": 80,
         "L100km": '10 Liter',
@@ -32,12 +54,12 @@ module.exports = [
         "model": "Pajero io",
         "transmission": "Automatic",
         "year": 1998,
-        "image": 'https://s3-eu-west-1.amazonaws.com/localrent.images/cars/image_titles/000/045/768/original/Hyundai-Tucson-2019-white.jpg?1686297153',
+        "image": 'https://s3-eu-west-1.amazonaws.com/localrent.images/cars/image_titles/000/027/349/original/Mitsubishi-PAJERO-io-2005-white.jpg?1652883725',
         "gallery": [
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/Hyndai/hyndai.jpeg',
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/Hyndai/hinday2.jpeg',
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/Hyndai/hyndai2.jpeg',
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/Hyndai/hyndai3.jpeg'
+            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/Pajeriio/pajerored1.jpeg',
+            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/Pajeriio/pajerored2.jpeg',
+            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/Pajeriio/pajerored3.jpeg',
+          
 
         ],
     }, {
@@ -63,16 +85,37 @@ module.exports = [
     }, {
         "Price": 200,
         "L100km": '10 Liter',
-        "class": "SUV",
+        "class": "Convertibles",
         "cylinders": 4,
-        "Engine": 2.4,
+        "Engine": 2.3,
         "drive": "fwd",
         "fuel_type": "Gasoline",
-        "make": "Chrysler",
-        "model": "Cruiser",
+        "make": "Mercedes ",
+        "model": "slk",
         "transmission": "Automatic",
-        "year": 2005,
-        "image": 'https://s3-eu-west-1.amazonaws.com/localrent.images/cars/image_titles/000/022/021/original/Chrysler-Cruiser-2005-red.jpg?1627467330',
+        "year": 2003,
+        "image": 'https://s3-eu-west-1.amazonaws.com/localrent.images/cars/image_titles/000/021/407/original/Mercedes-Benz-SLK-230-2006-silver.jpg?1624897419',
+        "gallery": [
+            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/Hyndai/hyndai.jpeg',
+            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/Hyndai/hinday2.jpeg',
+            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/Hyndai/hyndai2.jpeg',
+            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/Hyndai/hyndai3.jpeg'
+
+        ],
+    },
+    {
+        "Price": 200,
+        "L100km": '12-15 Liter',
+        "class": "SUV",
+        "cylinders": 6,
+        "Engine": 3.5,
+        "drive": "fwd",
+        "fuel_type": "Gasoline",
+        "make": "Toyota",
+        "model": "Sienna/Users/nikalachashvili/Desktop/Nika Cars/Toyota.jpeg /Users/nikalachashvili/Desktop/Nika Cars/Toyota1.jpeg /Users/nikalachashvili/Desktop/Nika Cars/Toyota3.jpeg",
+        "transmission": "Automatic",
+        "year": 2015,
+        "image": 'https://s3-eu-west-1.amazonaws.com/localrent.images/cars/image_titles/000/062/311/original/Toyota-Sienna-2016-black.jpg?1716792333',
         "gallery": [
             'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/Hyndai/hyndai.jpeg',
             'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/Hyndai/hinday2.jpeg',
